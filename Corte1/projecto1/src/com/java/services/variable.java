de5547d5bae3e51c6f69e0e97d619f4f0fa9c605
Mateo Gomez Sepulveda <mateos209000@gmail.com>
2024-08-11 19:52:26 -0500

    Create examples variables and delete pages ejercicio1.js and ejercicioPractico.java

---
+++ Corte1/projecto1/src/com/java/services/variable.java
@@ -0,0 +1,15 @@
+package com.java.services;
+
+public class variable {
+    public static void main(String[] args) {
+        int edad = 35;
+
+        System.out.println(edad );
+
+        double numeroDecimal = 3.14;
+        int numeroEntero = (int) numeroDecimal;
+
+        System.out.println(numeroEntero);
+    }
+}
+
